7a62ce153f756aa7a3693b710041900ba7ad93e1
김영은 <tqandyou@gmail.com>
2026-04-02 01:16:31 +0900

    Create knn.py

---
+++ ml_scratch/neighbors/knn.py
@@ -0,0 +1,28 @@
+import numpy as np
+from collections import Counter
+from ml_scratch.base import BaseEstimator
+
+class KNN(BaseEstimator):
+    def __init__(self, k=3):
+        self.k = k
+        self.X_train = None
+        self.y_train = None
+
+    def fit(self, X, y):
+        self.X_train = np.asarray(X, dtype=float)
+        self.y_train = np.asarray(y)
+        return self
+
+    def _euclidean_distance(self, x1, x2):
+        return np.sqrt(np.sum((x1 - x2) ** 2))
+
+    def predict(self, X):
+        X = np.asarray(X, dtype=float)
+        predictions = [self._predict_single(x) for x in X]
+        return np.array(predictions)
+
+    def _predict_single(self, x):
+        distances = [self._euclidean_distance(x, x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        return Counter(k_nearest_labels).most_common(1)[0][0]
